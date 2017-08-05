bf255679308eb4831b1f0d64ab204167870789e7
Liam Norman <liamnorman@Lees-MacBook-Pro.local>
2017-08-05 19:26:01 +0200

    added thread endpoints and tests, views, added travis config

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * GET /threads
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+        $response->assertStatus(200);
+
+    }
+
+    /**
+     * GET /threads/{id}
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function a_user_can_view_one_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+
+}
